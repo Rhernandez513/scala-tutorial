9c0a12a7afc0b3424b1c5504b28adacad97366a7
Robert David Hernandez <rhernandez513@gmail.com>
2017-12-14 19:26:31 -0600

    functions as first class objects and anonymous functions

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -3,3 +3,25 @@ object HelloWorld {
         println("Hello World!")
     }
 }
+
+object Timer {
+  def oncePerSecond (callback: () => Unit): Unit = {
+    while (true) { callback(); Thread sleep 1000 }
+  }
+  def timeFlies(): Unit = {
+    println("time flies like an arrow...")
+  }
+  def main(args: Array[String]): Unit = {
+    oncePerSecond(timeFlies);
+  }
+}
+
+object TimerAnonymous {
+  def oncePerSecond(callback: () => Unit): Unit = {
+    while (true) { callback(); Thread.sleep(1000) }
+  }
+  def main(args: Array[String]): Unit = {
+    oncePerSecond(() =>
+    println("time flies like an arrow..."))
+  }
+}
